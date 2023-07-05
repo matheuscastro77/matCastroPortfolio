6dabf7fd33f6251adb4d200bf7d976d89d225dc0
matheuscastro77 <matheustvcastro1997@gmail.com>
2023-07-05 10:32:54 -0300

    adding scrollTo method

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,82 +1,137 @@
 import React, { useRef } from "react";
-import { Main, Nav, Button, ButtonMenu, DivModal, NavMenu, ButtonClose} from "./styled";
+import {
+  Main,
+  Nav,
+  Button,
+  ButtonMenu,
+  DivModal,
+  NavMenu,
+  ButtonClose,
+} from "./styled";
 import About from "../../components/About/About";
 import Skills from "../../components/Skills/Skills";
 import Project from "../../components/Project/Project";
 import Contact from "../../components/Contact/Contact";
 import Start from "../../components/Start/Start";
-import MenuIcon from '@mui/icons-material/Menu';
+import MenuIcon from "@mui/icons-material/Menu";
 import { useContext } from "react";
-import GlobalStateContext from '../../context/GlobalStateContext'
-
+import GlobalStateContext from "../../context/GlobalStateContext";
 
 const Home = () => {
+  const { menu, setMenu } = useContext(GlobalStateContext);
 
-  const { menu, setMenu } = useContext(GlobalStateContext)
-
-  const ScrollStart = useRef(null);
+  const ScrollHome = useRef(null);
   const ScrollAbout = useRef(null);
   const ScrollAbility = useRef(null);
   const ScrollProject = useRef(null);
   const ScrollContact = useRef(null);
 
-  const scrollToStart = () => {
-    ScrollStart.current.scrollIntoView({ behavior: "smooth" })
-    setMenu(false)
-  }
+  const scrollToHome = () => {
+    window.scrollTo({
+      top: 0,
+      left: 0,
+      behavior: "smooth",
+    });
+    setMenu(false);
+  };
 
   const scrollToAbout = () => {
-    ScrollAbout.current.scrollIntoView({ behavior: "smooth" })
-    setMenu(false)
-  }
+    window.scrollTo({
+      top: 900,
+      left: 0,
+      behavior: "smooth",
+    });
+    setMenu(false);
+  };
 
   const scrollToAbility = () => {
-    ScrollAbility.current.scrollIntoView({ behavior: "smooth" })
-    setMenu(false)
-  }
+    window.scrollTo({
+      top: 1500,
+      left: 0,
+      behavior: "smooth",
+    });
+    setMenu(false);
+  };
 
   const scrollToProject = () => {
-    ScrollProject.current.scrollIntoView({ behavior: "smooth" })
-    setMenu(false)
-  }
+    window.scrollTo({
+      top: 2250,
+      left: 0,
+      behavior: "smooth",
+    });
+    setMenu(false);
+  };
 
   const scrollToContact = () => {
-    ScrollContact.current.scrollIntoView({ behavior: "smooth" })
-    setMenu(false)
-  }
+    window.scrollTo({
+      top: 2900,
+      left: 0,
+      behavior: "smooth",
+    });
+    setMenu(false);
+  };
+
+  const scrollToHomeMobile = () => {
+    ScrollHome.current.scrollIntoView({ behavior: "smooth" });
+    setMenu(false);
+  };
+
+  const scrollToAboutMobile = () => {
+    ScrollAbout.current.scrollIntoView({ behavior: "smooth" });
+    setMenu(false);
+  };
+
+  const scrollToAbilityMobile = () => {
+    ScrollAbility.current.scrollIntoView({ behavior: "smooth" });
+    setMenu(false);
+  };
+
+  const scrollToProjectMobile = () => {
+    ScrollProject.current.scrollIntoView({ behavior: "smooth" });
+    setMenu(false);
+  };
+
+  const scrollToContactMobile = () => {
+    ScrollContact.current.scrollIntoView({ behavior: "smooth" });
+    setMenu(false);
+  };
 
   const menuIsVisible = () => {
-    setMenu(!menu)
-  }
+    setMenu(!menu);
+  };
 
   return (
     <Main>
       <Nav>
         <div>
           <h1>Matheus Castro</h1>
-          <ButtonMenu onClick={() => menuIsVisible()}><MenuIcon sx={{ fontSize: '28px', color: 'white' }}/></ButtonMenu>
+          <ButtonMenu onClick={() => menuIsVisible()}>
+            <MenuIcon sx={{ fontSize: "28px", color: "white" }} />
+          </ButtonMenu>
           <container>
-          <Button onClick={scrollToStart}> Home </Button>
-          <Button onClick={scrollToAbout}> About </Button>
-          <Button onClick={scrollToAbility}> Skills </Button>
-          <Button onClick={scrollToProject}> Projects </Button>
-          <Button onClick={scrollToContact}> Contact </Button>
+            <Button onClick={scrollToHome}> Home </Button>
+            <Button onClick={scrollToAbout}> About </Button>
+            <Button onClick={scrollToAbility}> Skills </Button>
+            <Button onClick={scrollToProject}> Projects </Button>
+            <Button onClick={scrollToContact}> Contact </Button>
           </container>
         </div>
-        {menu ? <DivModal>
-          <ButtonClose onClick={() => menuIsVisible()}>X</ButtonClose>
-          <NavMenu>
-          <Button onClick={scrollToStart}> Home </Button>
-          <Button onClick={scrollToAbout}> About </Button>
-          <Button onClick={scrollToAbility}> Skills </Button>
-          <Button onClick={scrollToProject}> Projects </Button>
-          <Button onClick={scrollToContact}> Contact </Button>
-          </NavMenu>
-        </DivModal> : null}
+        {menu ? (
+          <DivModal>
+            <ButtonClose onClick={() => menuIsVisible()}>X</ButtonClose>
+            <NavMenu>
+              <Button onClick={scrollToHomeMobile}> Home </Button>
+              <Button onClick={scrollToAboutMobile}> About </Button>
+              <Button onClick={scrollToAbilityMobile}> Skills </Button>
+              <Button onClick={scrollToProjectMobile}> Projects </Button>
+              <Button onClick={scrollToContactMobile}> Contact </Button>
+            </NavMenu>
+          </DivModal>
+        ) : null}
       </Nav>
-        <div ref={ScrollStart}>
-          <Start />
-        </div>
+      <div ref={ScrollHome}>
+        <Start />
+      </div>
 
       <div ref={ScrollAbout}>
         {" "}
